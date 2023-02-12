8de1c08b8a2d78c358442ce8c95e2f4c68472d1a
kushwaha-github <kushwaha0sonu4008@gmail.com>
2023-02-12 13:32:14 +0530

    first commit

---
+++ src/test/java/com/hr/mgmt/EmployeeMgmtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgmtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
